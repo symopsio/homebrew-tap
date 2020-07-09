f94e0dda3d9ba84f2fe67d3f450562dc123f73e5
Yasyf Mohamedali <yasyfm@gmail.com>
2020-07-08 23:34:07 -0700

    Bump to 0.0.5.

--- Formula/sym.rb
+++ Formula/sym.rb
@@ -4,8 +4,8 @@ class Sym < Formula
   desc "The Sym CLI allows you to perform privileged operations with Sym."
   homepage "https://symops.com/"
 
-  url "https://files.pythonhosted.org/packages/9d/3b/82700b2eb48b68496541a626a2e340d24c9782bbf31842424105736c0383/sym-cli-0.0.3.tar.gz"
-  sha256 "8ab4a94a61ef89697b471109f249063fdea17bd36b44a293a22a7b36eb3b8b00"
+  url "https://files.pythonhosted.org/packages/3f/19/e3768aa6e38993b066c74f8319f7e35b7f15970e3de995702420c0213800/sym-cli-0.0.5.tar.gz"
+  sha256 "ef1362ec97ef8497661a3e256eafc4b798b020b0e8b61dacb8d2ef0cf16b5839"
 
   depends_on "python@3.8"
 
@@ -17,11 +17,6 @@ class Sym < Formula
   resource "PyYAML" do
     url "https://files.pythonhosted.org/packages/64/c2/b80047c7ac2478f9501676c988a5411ed5572f35d1beff9cae07d321512c/PyYAML-5.3.1.tar.gz"
     sha256 "b8eac752c5e14d3eca0e6dd9199cd627518cb5ec06add0de9d32baeee6fe645d"
-  end
-
-  resource "xdg" do
-    url "https://files.pythonhosted.org/packages/a4/54/923c5a30db7ce3df6aa4cc9ad785847d2356443781da85b8d769d4737622/xdg-4.0.1.tar.gz"
-    sha256 "c939c99def394cbaf765a3ee55efd6ea7e4c5eaed8d9ebc2d03af84ba35dec57"
   end
 
   def install
