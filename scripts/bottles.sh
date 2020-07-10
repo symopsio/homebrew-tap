377d5f92a22d8f5bbd034b730138151e16f43c5d
Yasyf Mohamedali <yasyfm@gmail.com>
2020-07-10 03:05:01 -0700

    Only push if upload succeeded.

--- scripts/bottles.sh
+++ scripts/bottles.sh
@@ -7,4 +7,7 @@ TAP=symopsio/tap
 cd `mktemp -d`
 
 brew test-bot --root-url=$BINTRAY_ROOT --bintray-org=$BINTRAY_USER --tap=$TAP $TAP/sym
-brew pr-upload --bintray-org=$BINTRAY_USER --root-url=$BINTRAY_ROOT || true
+
+if brew pr-upload --bintray-org=$BINTRAY_USER --root-url=$BINTRAY_ROOT; then
+  git push origin HEAD
+fi
