873c1bac550828ee0d16c7a507f6cc1eada69b23
Yasyf Mohamedali <yasyfm@gmail.com>
2020-07-10 01:09:58 -0700

    Create bottle in temp dir.

--- scripts/bottles.sh
+++ scripts/bottles.sh
@@ -4,5 +4,7 @@ BINTRAY_ROOT=https://dl.bintray.com/symopsio/bottles-tap
 BINTRAY_USER=symopsio
 TAP=symopsio/tap
 
+cd `mktemp -d`
+
 brew test-bot --root-url=$BINTRAY_ROOT --bintray-org=$BINTRAY_USER --tap=$TAP $TAP/sym
 brew pr-upload --bintray-org=$BINTRAY_USER --root-url=$BINTRAY_ROOT
