0c0a601f1de5b475a915ecd07a05674d351ae253
Yasyf Mohamedali <yasyfm@gmail.com>
2020-07-28 10:55:40 -0700

    Add uninstall to bottles script.

--- scripts/bottles.sh
+++ scripts/bottles.sh
@@ -4,6 +4,8 @@ BINTRAY_ROOT=https://dl.bintray.com/symopsio/bottles-tap
 BINTRAY_USER=symopsio
 TAP=symopsio/tap
 
+brew uninstall sym
+
 cd `mktemp -d`
 
 brew test-bot --root-url=$BINTRAY_ROOT --bintray-org=$BINTRAY_USER --tap=$TAP $TAP/sym
