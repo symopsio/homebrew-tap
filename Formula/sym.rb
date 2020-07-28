3c88804d3fe4b8ca4b60c50ce3d506a3ec8389c4
Yasyf Mohamedali <yasyfm@gmail.com>
2020-07-27 22:44:27 -0700

    Add cataline.

--- Formula/sym.rb
+++ Formula/sym.rb
@@ -11,6 +11,7 @@ class Sym < Formula
     root_url "https://dl.bintray.com/symopsio/bottles-tap"
     cellar :any
     sha256 "29b3479e09c165e27506d29ca233afd5cf6eb24ad9a2e312ac547a2c73b6af4c" => :mojave
+    sha256 "c921c1fa31e08ec9f8526a4c563e5f846c0c60145e142487fdbf53e7ba9f509e" => :catalina
   end
 
   depends_on "python@3.8"
