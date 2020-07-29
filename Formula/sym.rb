b35436dbd9fbea85544acc6487c39cca984137b0
Yasyf Mohamedali <yasyfm@gmail.com>
2020-07-28 18:14:16 -0700

    Add catalina bottle.

--- Formula/sym.rb
+++ Formula/sym.rb
@@ -11,6 +11,7 @@ class Sym < Formula
     root_url "https://dl.bintray.com/symopsio/bottles-tap"
     cellar :any
     sha256 "046f487e8847184eddc768aed1ff50c7c8d0ab167774bca83a7efc005a887664" => :mojave
+    sha256 "03a7784de07bf31e69342fb86063888078cd473c61ce4eb3b959283f656fd233" => :catalina
   end
 
   depends_on "python@3.8"
--- scripts/bottles.sh
+++ scripts/bottles.sh
@@ -6,8 +6,6 @@ TAP=symopsio/tap
 
 brew uninstall sym
 
-cd `mktemp -d`
-
 brew test-bot --root-url=$BINTRAY_ROOT --bintray-org=$BINTRAY_USER --tap=$TAP $TAP/sym
 
 if brew pr-upload --bintray-org=$BINTRAY_USER --root-url=$BINTRAY_ROOT; then
