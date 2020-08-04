b2c17195be5f14b84c8890c9024a108f3adebc58
Yasyf Mohamedali <yasyfm@gmail.com>
2020-08-03 17:08:48 -0700

    sym: update 0.0.13 bottle.

--- Formula/sym.rb
+++ Formula/sym.rb
@@ -10,6 +10,7 @@ class Sym < Formula
   bottle do
     root_url "https://dl.bintray.com/symopsio/bottles-tap"
     cellar :any
+    sha256 "f1235a3759779df9602a2ef7761537ecdb9f5b3de550bfcab92c656649b0aa77" => :mojave
   end
 
   depends_on "python@3.8"
