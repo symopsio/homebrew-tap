91b1bfe9cd43e5257809a6c74b0c2bfe6169207f
Sym CI <ci@symops.io>
2022-01-10 21:57:05 +0000

    [auto] Update sym-cli to 0.2.3.

--- Formula/sym@0.2.3.rb
+++ Formula/sym@0.2.3.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # THIS FILE IS GENERATED. DO NOT EDIT DIRECTLY.
 
-class Sym < Formula
+class SymAT023 < Formula
   desc "CLI to perform privileged operations with Sym"
   homepage "https://docs.symops.com"
   version "0.2.3"
