d71262a33d94b61ec5ce9d6145c7b764748d1724
Sym CI <ci@symops.io>
2022-11-01 13:21:26 +0000

    [auto] Update sym-flow-cli to 4.1.0.

--- Formula/symflow@4.1.0.rb
+++ Formula/symflow@4.1.0.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # THIS FILE IS GENERATED. DO NOT EDIT DIRECTLY.
 
-class SymflowAT4 < Formula
+class SymflowAT410 < Formula
   desc "CLI to deploy Sym Flows"
   homepage "https://docs.symops.com"
   version "4.1.0"
