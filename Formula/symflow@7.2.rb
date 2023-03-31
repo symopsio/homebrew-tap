5ea26bd02287c23896cefb52bca8dfc6b0b7c783
Sym CI <ci@symops.io>
2023-03-31 13:39:08 +0000

    [auto] Update sym-flow-cli to 7.2.0.

--- Formula/symflow@7.2.rb
+++ Formula/symflow@7.2.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # THIS FILE IS GENERATED. DO NOT EDIT DIRECTLY.
 
-class Symflow < Formula
+class SymflowAT72 < Formula
   desc "CLI to deploy Sym Flows"
   homepage "https://docs.symops.com"
   version "7.2.0"
